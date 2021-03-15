cb54fd996043b16db0df2bd83e27ac63c5e3e851
Alexey Struchenkov <forcorp@inbox.ru>
2021-03-15 23:12:51 +0200

    init commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='container mt-2'>
+        <header>
+          <h1>App</h1>
+        </header>
+        <form action='' className='bordered'>
+          some inputs here...
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
