2f630c5def96583c680cd6a3d40e46fd2168baf8
Alexey Struchenkov <forcorp@inbox.ru>
2021-04-07 12:01:18 +0200

    Добавлен TODO для теста

--- src/pages/FormPage/FormPage.tsx
+++ src/pages/FormPage/FormPage.tsx
@@ -3,19 +3,19 @@ import FormInputText from '../../components/FormInputText';
 import FormInputFile from '../../components/FormInputFile';
 import FormSubmit from '../../components/FormSubmit';
 import FormResponse from '../../components/FormResponse';
-import {API_URL, HttpResponse, paramsJsonToFormData, postFormData} from '../../utils/api';
+import { API_URL, HttpResponse, paramsJsonToFormData, postFormData } from '../../utils/api';
 
+// TODO: Добавить свойства
 interface IFromPageProps {}
 
 type RequestParamType = {
   action: 'send_data';
-  id: 1,
+  id: 1;
   contact: string;
   image: File | null;
 };
 
-
-const FromPage: React.FunctionComponent<IFromPageProps> = (props) => {
+const FromPage: React.FunctionComponent<IFromPageProps> = () => {
   const [name, setName] = useState('');
   const [lastName, setLastName] = useState('');
   const [patronymic, setPatronymic] = useState('');
@@ -37,11 +37,11 @@ const FromPage: React.FunctionComponent<IFromPageProps> = (props) => {
   useEffect(() => {
     const fetchData = async (requestParams: RequestParamType) => {
       setLoading(true);
-      const formData = paramsJsonToFormData(requestParams)
-      const res:HttpResponse<object> = await postFormData(API_URL, formData);
+      const formData = paramsJsonToFormData(requestParams);
+      const res: HttpResponse<any> = await postFormData(API_URL, formData);
       setLoading(false);
       setLoaded(true);
-      setResponse(res.parsedBody)
+      setResponse(res.parsedBody);
     };
     if (!loaded) {
       fetchData(requestParams);
@@ -62,10 +62,16 @@ const FromPage: React.FunctionComponent<IFromPageProps> = (props) => {
       <form className='col s6' onSubmit={handleSubmit}>
         <FormInputText id='name' label='Имя' className='white-text' value={name} onChange={setName} />
         <FormInputText id='lastName' label='Фамилия' className='white-text' value={lastName} onChange={setLastName} />
-        <FormInputText id='patronymic' label='Отчество' className='white-text' value={patronymic} onChange={setPatronymic} />
+        <FormInputText
+          id='patronymic'
+          label='Отчество'
+          className='white-text'
+          value={patronymic}
+          onChange={setPatronymic}
+        />
         <FormInputFile files={files} onChange={handleImageChange} />
-        <FormSubmit onSubmit={handleSubmit} loading={loading}/>
-        <FormResponse response={ Object.keys(response).length ? JSON.stringify(response, null, 4) : ''} />
+        <FormSubmit onSubmit={handleSubmit} loading={loading} />
+        <FormResponse response={Object.keys(response).length ? JSON.stringify(response, null, 4) : ''} />
       </form>
     </div>
   );
